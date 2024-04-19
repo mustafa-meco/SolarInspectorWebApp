496d474a36803d4e08c7de99e24e2e1a7378e209
Restyled.io <commits@restyled.io>
2024-04-19 11:33:57 +0000

    Restyled by pyment

--- shared/utils/helpers.py
+++ shared/utils/helpers.py
@@ -2,12 +2,29 @@
 
 
 def add_numbers(a, b):
+    """
+
+    :param a: 
+    :param b: 
+
+    """
     return a + b
 
 
 def multiply_numbers(a, b):
+    """
+
+    :param a: 
+    :param b: 
+
+    """
     return a * b
 
 
 def greet_user(name):
+    """
+
+    :param name: 
+
+    """
     return f"Hello, {name}!"
